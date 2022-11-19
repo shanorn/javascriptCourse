1574926ceb5dbf8567dcc392263f820cf409738f
shanorn <shanorn@gmail.com>
2022-11-19 18:23:39 +0100

    PreviousPractice

---
+++ 01 Fundamentals P1/script.js
@@ -0,0 +1,29 @@
+let js = 'amazing';
+console.log(40 + 8 + 13 - 10);
+
+console.log('Jonas');
+console.log(23);
+
+let firstName = 'Matilda';
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+// Only can use _ or $ in variable 
+let jonas_matilda = 'JM';
+let $function = 27;
+
+let person = "jonas";
+
+// Mayus for constants convention
+let PI = 3.1415;
+
+// Goob
+let myFirstJob = 'Coder';
+let myCurrentJob = 'Teacher';
+
+// Bad 
+let job1 = 'Programmer';
+let job2 = 'Teacher';
+
+console.log(myFirstJob);
