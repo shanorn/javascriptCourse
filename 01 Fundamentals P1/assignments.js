13b05ba665a501ba49e90899486341a753ad02b3
shanorn <shanorn@gmail.com>
2022-11-19 18:56:41 +0100

    Values and Variables

---
+++ 01 Fundamentals P1/assignments.js
@@ -0,0 +1,10 @@
+// Lecture: Values and Variables
+let country = 'Spain';
+let continent = 'Europe';
+let population = 47.33;
+
+console.log(country);
+console.log(continent);
+console.log(population);
+
+console.log('I live in ' + country + ' (' + continent + ') and it has ' + population + ' million people');
