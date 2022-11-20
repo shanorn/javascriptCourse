35d905b2810af7637f868d8cfad460f4c05f9315
shanorn <shanorn@gmail.com>
2022-11-20 15:22:29 +0100

    Code Challenge #1

--- 01 Fundamentals P1/script.js
+++ 01 Fundamentals P1/script.js
@@ -78,7 +78,7 @@ console.log(job);
 
 
 
-/* Lecture: Basic Operators */
+/* Lecture: Basic Operators 
 
 // Math Operators
 const now = 2037;
@@ -111,4 +111,20 @@ console.log(ageSarah >= 18);
 
 const isFullAge = ageSarah >= 18;
 
-console.log(now - 1991 > now -2018)
+console.log(now - 1991 > now -2018);
+*/
+
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+
+console.log(now - 1991 > now -2018);
+
+console.log(25 - 10 - 5);
+
+let x, y; 
+x = y = 25-10-5;
+console.log(x, y);
+
+const averageAge = (ageJonas + ageSarah) / 2;
+console.log(ageJonas, ageSarah, averageAge);
