823270b162082d468638b8db9c6367dc5859829a
shanorn <shanorn@gmail.com>
2022-11-20 13:32:31 +0100

    Data Types

--- 01 Fundamentals P1/assignments.js
+++ 01 Fundamentals P1/assignments.js
@@ -8,3 +8,13 @@ console.log(continent);
 console.log(population);
 
 console.log('I live in ' + country + ' (' + continent + ') and it has ' + population + ' million people');
+
+
+// Lecture: Data Types
+let isIsland = false;
+let language;
+
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
--- 01 Fundamentals P1/script.js
+++ 01 Fundamentals P1/script.js
@@ -1,3 +1,5 @@
+/* Values and Variables
+
 let js = 'amazing';
 console.log(40 + 8 + 13 - 10);
 
@@ -27,3 +29,29 @@ let job1 = 'Programmer';
 let job2 = 'Teacher';
 
 console.log(myFirstJob);
+*/
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+//console.log(typeof true);
+console.log(typeof javascriptIsFun);
+//console.log(typeof 38.23);
+//console.log(typeof 'cadena');
+
+javascriptIsFun = 'YES!';
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1989;
+console.log(year);
+console.log(typeof year);
+
+year = undefined;
+console.log(year);
+console.log(typeof year);
+
+console.log(typeof null);
