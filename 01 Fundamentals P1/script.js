64060db850a80be0fd5b7bf2d2ceb340023068bd
shanorn <shanorn@gmail.com>
2022-11-21 23:57:19 +0100

    quality Operators

--- 01 Fundamentals P1/script.js
+++ 01 Fundamentals P1/script.js
@@ -205,7 +205,7 @@ console.log('10' - '4' - '3' - 2 +'5'); // 15
 
 */ 
 
-/* Truthy and Falsy Values */
+/* Truthy and Falsy Values 
 
 // 5 falsy values: 0, '', undefined, null, NaN
 
@@ -233,5 +233,34 @@ if (height) {
 } else {
     console.log('Height is UNDEFINED')
 }
+*/
 
- 
+/* Equality Operators */ 
+
+const age = 18;
+if (age === 18) console.log('Exactly 18 (strict)');
+
+const stringAge = '18';
+if (age == 18) console.log('Exactly 18 (loose)');
+
+const favorite = Number(prompt("What's your favorite number?"));
+console.log(favorite);
+console.log(typeof favorite);
+console.log(Number(favorite));
+console.log(typeof Number(favorite));
+
+if (favorite == 23) {
+    console.log('Cool! 23 is an amazing number!!');
+}
+
+if (favorite === 23) {
+    console.log('Cool! 23 is an amazing number!!');
+} else if (favorite === 7){
+    console.log('7 is also a cool number');
+} else if (favorite === 9){
+    console.log('9 is also a cool number');
+} else {
+    console.log('Number is not 23 or 7 or 9');
+}
+
+if (favorite !== 23) console.log("Why not 23?");
