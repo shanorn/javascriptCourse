cecf820c128caf11d747cd24a787c292f507d1ac
shanorn <shanorn@gmail.com>
2022-11-25 00:38:21 +0100

    Coding Challenge #4

--- Coding Challenges/Coding Challenge #4/cc04.js
+++ Coding Challenges/Coding Challenge #4/cc04.js
@@ -25,3 +25,6 @@ Hints:
 
 GOOD LUCK 😀 */
 
+const bill = 430; 
+const tip = bill >= 50 && bill <= 300 ? 0.15*bill: 0.20*bill;
+console.log(`The bill was ${bill}, the tip was ${tip}, and the total value ${bill+tip}`);
