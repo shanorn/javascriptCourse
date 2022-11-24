b1f5b63721cdacfe60b9504290b9e1990dbd7a11
shanorn <shanorn@gmail.com>
2022-11-25 00:23:01 +0100

    Ternary operator

--- 01 Fundamentals P1/script.js
+++ 01 Fundamentals P1/script.js
@@ -338,3 +338,39 @@ if (day === 'monday') {
 }
 */
 
+/* Statements and expressions 
+
+//Expressions
+3 + 4 
+1991
+true && false && !false
+
+//Statements
+if (23 > 10) {
+    const str = '23 is bigger';
+}
+
+const me = 'Cristian';
+console.log(`I'm ${2037 - 1991} years old ${me}`);
+
+*/
+
+/* The conditional (ternary) operator 
+
+const age = 12;
+
+age >= 18 ? console.log('I like to drink wine') : console.log('I like to drink water');
+
+const exDrink = age >= 18 ? 'wine' : 'water';
+console.log(exDrink);
+
+let exDrink2;
+if (age >= 18){
+    exDrink2 = 'wine';
+} else {
+    exDrink2 = 'water';
+}
+console.log(exDrink2);
+
+console.log(`I like to drink ${age >= 18 ? 'wine!' : 'water T_T'}`);
+*/
