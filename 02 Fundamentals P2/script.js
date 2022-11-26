280b9477a53398ee8d5c33379c24bb9b66057ded
shanorn <shanorn@gmail.com>
2022-11-26 13:01:53 +0100

    Function calling others functions

--- 02 Fundamentals P2/script.js
+++ 02 Fundamentals P2/script.js
@@ -82,3 +82,20 @@ console.log(yearsUntilRetirement(1989, 'Cristian'));
 console.log(yearsUntilRetirement(1992, 'Merces'));
 
 */
+
+
+/* Function calling others functions */
+
+
+function cutFruitPieces(fruit){
+    return fruit * 4;
+}
+function fruitProcessor(apples, oranges) {
+    //console.log(apples, oranges);
+    const applePieces = cutFruitPieces(apples);
+    const orangePieces = cutFruitPieces(oranges);
+    const juice = `Juice with ${applePieces} pieces of apples and ${orangePieces} pieces of oranges.`;
+    return juice; 
+}
+
+console.log(fruitProcessor(2, 3));
