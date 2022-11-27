8ab5b89a7a7034cd3dfd9dcf02be7398226e31f7
shanorn <shanorn@gmail.com>
2022-11-28 00:31:37 +0100

    Developer Skills

---
+++ 03 Developer Skills/script.js
@@ -0,0 +1,10 @@
+// Remember, we're gonna use strict mode in all scripts now!
+'use strict';
+
+const x = 23;
+
+if (x === 23) console.log(x);
+
+const calAge = birthYear => 2022 - birthYear;
+
+console.log(x);
