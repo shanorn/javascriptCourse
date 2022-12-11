6276592777d067398feffe54d31ea64683d466c9
shanorn <shanorn@gmail.com>
2022-12-11 20:10:02 +0100

    Handling Click Events

--- 05-Guess-My-Number/script.js
+++ 05-Guess-My-Number/script.js
@@ -1,5 +1,5 @@
 'use strict';
-
+/*
 console.log(document.querySelector('.message').textContent);
 
 document.querySelector('.message').textContent = 'Correct Number!';
@@ -11,4 +11,14 @@ document.querySelector('.score').textContent = 10;
 
 document.querySelector('.guess').value = 23;
 
-console.log(document.querySelector('.guess').value);
+console.log(document.querySelector('.guess').value);*/
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = '⛔ No Number';
+  }
+});
